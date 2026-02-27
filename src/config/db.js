5d408e136b9f2d7dcb7f6985bb68c9dd96323eff
parfaitBashombe <parfaitbashombe@gmail.com>
2026-02-27 23:47:52 +0300

    fx(prisma):error

--- src/config/db.js
+++ src/config/db.js
@@ -1,6 +1,12 @@
 import { PrismaClient } from "@prisma/client";
+import { Pool } from "pg";
+import { PrismaPg } from "@prisma/adapter-pg";
+
+const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+const adapter = new PrismaPg(pool);
 
 const prisma = new PrismaClient({
+  adapter,
   log:
     process.env.NODE_ENV === "development"
       ? ["query", "error", "warn"]
