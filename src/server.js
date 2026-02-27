1e0debe5ee902084c7e130242f70ff59a3ba269c
parfaitBashombe <parfaitbashombe@gmail.com>
2026-02-27 19:07:25 +0300

    ft(db):connected

--- src/server.js
+++ src/server.js
@@ -1,7 +1,12 @@
 import express from "express";
+import { config } from "dotenv";
+import { connectDB, disconnectDB } from "./config/db.js";
 
 //Import routes
 import movies from "../src/routes/movies.js";
+
+config();
+connectDB();
 
 const app = express();
 
@@ -12,4 +17,29 @@ const PORT = 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on PORT ${PORT}`);
+});
+
+//Handle unhadled promise rejections
+process.on("unhandleRejection", (err) => {
+  console.error("Unhandled Rejection:", err);
+  server.close(async () => {
+    await disconnectDB();
+    process.exit(1);
+  });
+});
+
+//Handle uncaught exceptions
+process.on("uncaughtException", async (err) => {
+  console.error("Uncaught Exception:", err);
+  await disconnectDB();
+  process.exit(1);
+});
+
+//Graceful shutdown
+process.on("SIGTERM", async () => {
+  console.log("SIGTERM received, shutting down gracefully");
+  server.close(async () => {
+    await disconnectDB();
+    process.exit(0);
+  });
 });
